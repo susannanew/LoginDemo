7049278c5ba587a698ff19963b388138d3c4ff29
susannanew <susannasameh01@gmail.com>
2021-12-08 01:34:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/logindemo/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.logindemo
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    var callback: CallbackManager? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        callback= CallbackManager.Factory.create()
+        loginButton.registerCallback(callback, object : FacebookCallback<LoginResult> {
+            override fun onCancel() {
+                textView.text =" login canceled"
+            }
+
+            override fun onError(error: FacebookException) {
+            }
+
+            override fun onSuccess(result: LoginResult) {
+                textView.text= "Login Success${result?.accessToken?.userId}"+
+                        "${result?.accessToken?.token}"
+            }
+            
+
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callback?.onActivityResult(requestCode,resultCode,data)
+
+    }
+}
